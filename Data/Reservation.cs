d4ddfeeb422d6f563126e439143406a93eac14e3
Jesse Munro <128737662+munroajesse@users.noreply.github.com>
2023-07-06 12:18:26 -0600

    Add files via upload

---
+++ Data/Reservation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Assignment3.Data
+{
+    public class Reservation
+    {
+        public string ReservationCode { get; set; }
+        public Flight SelectedFlight { get; set; }
+        public string FullName { get; set; }
+        public string Citizenship { get; set; }
+        public string Status { get; set; }
+
+
+        public Reservation(string reservationCode, Flight selectedFlight, string fullName, string citizenship, string status)
+        {
+            ReservationCode = reservationCode;
+            SelectedFlight = selectedFlight;
+            FullName = fullName;
+            Citizenship = citizenship;
+            Status = status;
+        }
+    }
+}
